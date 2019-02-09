9531e9cdb29930c7383ecdf17d07e090cf97b928
dante-signal31 <dante.signal31@gmail.com>
2019-02-09 23:40:08 +0100

    Scaffolding of main function at lib.rs started.

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,12 @@ use configuration::Configuration;
 use std::error::Error;
 
 /// Main function in steganer. It runs its main logic.
-pub fn run(config: Configuration)-> Result<(), Box<dyn Error>{
-
+pub fn run(config: Configuration)-> Result<(), Box<dyn Error>>{
+    if config.extract {
+        extract(config)
+    } else {
+        hide(config)
+    }
 }
 
 /// Create a configuration struct.
@@ -12,4 +16,12 @@ pub fn run(config: Configuration)-> Result<(), Box<dyn Error>{
 /// run function.
 pub fn create_configuration(file_hidden: String, host_file: String, extract: bool)-> Configuration {
     Configuration::new(file_hidden, host_file, extract)
+}
+
+fn extract(config: Configuration)-> Result<(), Box<dyn Error>>{
+    Ok(())
+}
+
+fn hide(config: Configuration)-> Result<(), Box<dyn Error>>{
+    Ok(())
 }
