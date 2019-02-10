7c0fa5da7468291a791cf788a266788ff95806bd
dante-signal31 <dante.signal31@gmail.com>
2019-02-10 12:30:49 +0100

    Added some docstrings to encode and extract functions.

--- src/lib.rs
+++ src/lib.rs
@@ -18,10 +18,24 @@ pub fn create_configuration(hidden_file: String, host_file: String, extract: boo
     Configuration::new(hidden_file, host_file, extract)
 }
 
-fn extract(hidden_file: String, host_file: String)-> Result<(), Box<dyn Error>>{
+/// Encode a file into another using steganography techniques.
+///
+/// Returns a boxed Error if something bad happens.
+///
+/// # Parameters:
+/// * hidden_file: Absolute path to file to hide.
+/// * host_file: Absolute path to file that is going to contain hidden file.
+pub fn extract(hidden_file: String, host_file: String)-> Result<(), Box<dyn Error>>{
     Ok(())
 }
 
-fn hide(hidden_file: String, host_file: String)-> Result<(), Box<dyn Error>>{
+/// Decode a file hidden into another using steganography techniques.
+///
+/// Returns a boxed Error if something bad happens.
+///
+/// # Parameters:
+/// * hidden_file: Absolute path to hidden file.
+/// * host_file: Absolute path to file that contains hidden file.
+pub fn hide(hidden_file: String, host_file: String)-> Result<(), Box<dyn Error>>{
     Ok(())
 }
