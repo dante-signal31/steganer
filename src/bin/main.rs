505d80086ac0fd9a2be0d19b7ef274e9638de025
dante-signal31 <dante.signal31@gmail.com>
2019-05-11 23:35:46 +0200

    Removed debug line.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -6,7 +6,6 @@ use steganer::_run;
 
 fn main() {
     let config = parse_arguments();
-    println!("Parsed data: {:?}", config);
     match _run(&config) {
         Ok(())=> std::process::exit(0),
         _ => std::process::exit(1),
